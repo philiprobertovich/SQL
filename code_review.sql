82c4717f95d95d2fd6395fb2b5a9ed3f6c0b0ee2
Phil Kendall <philiprobertovich@gmail.com>
2022-12-18 09:40:59 -0800

    complete aggregate function portion of code review.

--- code_review.sql
+++ code_review.sql
@@ -19,3 +19,9 @@ WHERE inventory_id IN (SELECT id FROM inventories WHERE version > 1) LIMIT 10;
 -- Manipulate Values in Select
 SELECT s.year, UPPER(CONCAT(s.name, '!!!')) FROM sets AS s WHERE name LIKE '%Batman%';
 SELECT quantity * 2 FROM inventory_parts WHERE quantity > 1 ORDER BY quantity LIMIT 20;
+
+-- Aggregation Functions
+SELECT COUNT(is_trans) FROM colors WHERE is_trans = 1;
+SELECT SUM(num_parts) FROM sets WHERE year >= 2000;
+SELECT AVG(num_parts) FROM sets WHERE year >= 2000;
+SELECT theme_id, AVG(num_parts) FROM sets GROUP BY theme_id;
