e3da029c67bcf84de794159d6bc8c1c171781c75
Phil Kendall <philiprobertovich@gmail.com>
2022-12-18 06:59:09 -0800

    complete subquery portion of code review.

--- code_review.sql
+++ code_review.sql
@@ -2,7 +2,16 @@
 show databases;
 use lego;
 show tables;
+
 -- Selecting values
 SELECT name, part_num FROM parts WHERE name LIKE '%Hair%' ORDER BY part_num DESC;
 SELECT * FROM sets WHERE name LIKE '%Showdown%' AND year BETWEEN 1990 AND 2015;
 SELECT p.id, p.name FROM part_categories AS p WHERE name LIKE '%Bricks%';
+
+-- Subqueries
+SELECT *
+FROM sets
+WHERE theme_id IN (SELECT id FROM themes WHERE name IN ('Pirates', 'Star Wars'));
+SELECT * 
+FROM inventory_parts
+WHERE inventory_id IN (SELECT id FROM inventories WHERE version > 1) LIMIT 10;
